d453b466083276bea8161583a0c018356c0bfa47
bt24cse141-netizen <bt24cse141@iiitn.ac.in>
2025-10-31 16:10:55 +0530

    Update 9.sh

--- Questions to be uploaded/9.sh
+++ Questions to be uploaded/9.sh
@@ -1,24 +1,21 @@
 #!/bin/bash
 
-total_width=18  # total width of the line
+total_width=18  
 
 for ((i=1; i<=9; i++))
 do
-    # Build ascending numbers
     asc=""
     for ((j=1; j<=i; j++))
     do
         asc+="$j"
     done
 
-    # Build descending numbers
     desc=""
     for ((j=i; j>=1; j--))
     do
         desc+="$j"
     done
 
-    # Calculate spaces needed
     spaces=$((total_width - ${#asc} - ${#desc}))
     space_str=""
     for ((k=0; k<spaces; k++))
@@ -26,6 +23,5 @@ do
         space_str+=" "
     done
 
-    # Print line
     echo "${asc}${space_str}${desc}"
 done
